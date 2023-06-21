bed9b5ead69bf450be61b183421f75576f851c60
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2023-06-21 12:03:55 +0900

    チャプター61

---
+++ Source/UEC_Lecture/Private/CPPStatic.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPStatic.h"
+
+
+// Satic•Ï”‚ğ‰Šú‰»‚·‚é
+int ACPPStatic::staticPoint = 400;
+
+void ACPPStatic::SetPoint(int myPoint)
+{
+	staticPoint = myPoint;
+}
---
+++ Source/UEC_Lecture/Public/CPPStatic.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPPStatic.generated.h"
+
+UCLASS()
+class UEC_LECTURE_API ACPPStatic : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	// 静的メンバ変数
+	static int staticPoint;
+
+	// 通常のメンバ変数
+	int normalPoint;
+
+	// staticメンバ関数
+	static void SetPoint(int myPoint);
+
+};
